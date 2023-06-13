c660182232c74f3e1ae919f8c364091e28265acc
Hanna Borgli <borgli.hanna@gmail.com>
2023-06-13 20:58:30 +0200

    Made various changes to logging

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import argparse
 import json
+import platform
+import re
 import sys
+
+import psutil
+import torch
 
 from sam_with_clip import segment_video
 import os
@@ -9,6 +14,32 @@ from pathlib import Path
 
 
 def run_segment_video(log_folder_path):
+    # Get GPU info
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    gpu_info = {}
+    if device.type == "cuda":
+        gpu_info = {
+            "name": torch.cuda.get_device_name(0),
+            "count": torch.cuda.device_count(),
+            "memory": torch.cuda.get_device_properties(0).total_memory / 1e9  # in GB
+        }
+
+    # Get system info
+    system_info = {
+        "platform": platform.platform(),
+        "machine": platform.machine(),
+        "processor": platform.processor(),
+        "num_cores": psutil.cpu_count(),
+        "frequency": psutil.cpu_freq(),
+        "gpu": gpu_info
+    }
+    print(f"System info: {system_info}")
+
+    # Update the name of the log folder path
+    log_folder_path = Path(log_folder_path).rename(Path(
+        f"{log_folder_path}_{gpu_info.get('name', platform.processor())}_{gpu_info.get('count', psutil.cpu_count())}_units").resolve())
+    print(f"Changed log folder name to '{log_folder_path}'")
+
     # Run script in another process called resource_monitor.py and send in the path of the log folder
     resource_monitor_process = subprocess.Popen([sys.executable, "resource_monitor.py", "--log_dir", str(log_folder_path)])
 
@@ -26,9 +57,13 @@ def run_segment_video(log_folder_path):
 
     # Stop the resource_monitor.py script
     resource_monitor_process.terminate()
-
+    results = list(filter(lambda x: re.match(r"\d+-(.*)"), os.listdir(log_folder_path)))
+    environment = re.search(r"\d+-(.*)", results[0]).group(1)
+    Path(log_folder_path).rename(Path(log_folder_path).joinpath(f"{environment}"))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("log_folder_path")
-    run_segment_video(parser.log_folder_path)
+    args = parser.parse_args()
+    print(f"Started running main task for folder {args.log_folder_path}")
+    run_segment_video(args.log_folder_path)
--- run_experiment.py
+++ run_experiment.py
@@ -1,26 +1,13 @@
 import json
+import os
 import shutil
 import subprocess
 import datetime
-import platform
 from pathlib import Path
-
-import psutil
-import torch
 
 if __name__ == '__main__':
     # Generate experiment ID from current time
     exp_id = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-
-    # Get GPU info
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    gpu_info = {}
-    if device.type == "cuda":
-        gpu_info = {
-            "name": torch.cuda.get_device_name(0),
-            "count": torch.cuda.device_count(),
-            "memory": torch.cuda.get_device_properties(0).total_memory / 1e9  # in GB
-        }
 
     print(f"Starting experiment: {exp_id}")
 
@@ -31,35 +18,31 @@ if __name__ == '__main__':
         "query": "polyp"
     }
 
-    # Get system info
-    system_info = {
-        "platform": platform.platform(),
-        "machine": platform.machine(),
-        "processor": platform.processor(),
-        "num_cores": psutil.cpu_count(),
-        "frequency": psutil.cpu_freq(),
-        "gpu": gpu_info
-    }
-    print(f"System info: {system_info}")
-
     # Create log folder with the name of the experiment ID and include the GPU model name
-    log_folder_path = Path(
-        f"{exp_id}_{gpu_info.get('name', platform.processor())}_{gpu_info.get('count', psutil.cpu_count)}").resolve()
-    print(f"Creating log folders at {log_folder_path}")
+    log_folder_path = Path(f"{exp_id}").resolve()
+    print(f"Creating log folders at '{log_folder_path}'")
     log_folder_path.mkdir(parents=True, exist_ok=True)
 
     with open(Path(log_folder_path).joinpath("hyperparameters.json"), 'w') as f:
         json.dump(hyperparameters, f)
 
-    with open(Path(log_folder_path).joinpath("system_info.json"), 'w') as f:
-        json.dump(system_info, f)
-
     if Path("run_sam_with_clip.sh").exists():
         shutil.copy2(Path("run_sam_with_clip.sh"), Path(log_folder_path).joinpath("run_sam_with_clip.sh"))
 
-    experiment = subprocess.Popen([Path(log_folder_path).joinpath("run_sam_with_clip.sh").resolve(), log_folder_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    experiment = subprocess.Popen(
+        ["sbatch", "--parsable", Path(log_folder_path).joinpath("run_sam_with_clip.sh"), log_folder_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = experiment.communicate()
-    print(stdout)
-    print(stderr)
+    jobid = stdout.decode().strip()
+    if jobid.isdigit():
+        node = subprocess.Popen(
+            ["squeue", "-h", "-o", '"%P"', f"-j{jobid}"],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        nodeout, nodeerr = node.communicate()
+        info = nodeout.decode().strip().strip('"')
+        print(f"Job {jobid} added to the queue on partition '{info}'. ")
+        with open(Path(log_folder_path).joinpath(f"{jobid}-{info}"), "w") as f:
+            f.write(f"{jobid}-{info}")
+    if stderr.decode():
+        print(stderr.decode())
 
 
