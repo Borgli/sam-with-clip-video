9ceeb8e487894793c74682e4fb7447f708d04f17
Hanna Borgli <borgli.hanna@gmail.com>
2023-06-13 14:38:01 +0200

    Changed so output is saved to correct log

--- main.py
+++ main.py
@@ -1,85 +1,19 @@
+import argparse
 import json
 import sys
 
 from sam_with_clip import segment_video
-
-import psutil
-import torch.multiprocessing as mp
 import os
 import subprocess
-import platform
-import datetime
-import torch
 from pathlib import Path
 
 
-def run_benchmark():
-    mp.set_start_method("spawn", force=True)
-
-    p = mp.Process(target=segment_video,
-                   kwargs={
-                       "predicted_iou_threshold": 0.9,
-                       "stability_score_threshold": 0.8,
-                       "clip_threshold": 0.9,
-                       "video_path": "video.avi",
-                       "query": "polyp",
-                       "output_path": "output.avi"
-                   }
-                   )
-
-    p.start()
-    print("Started")
-    p.join()
-    print("Finished")
-
-
-def run_segment_video():
-    # Generate experiment ID from current time
-    exp_id = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-
-    # Get GPU info
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    gpu_info = {}
-    if device.type == "cuda":
-        gpu_info = {
-            "name": torch.cuda.get_device_name(0),
-            "count": torch.cuda.device_count(),
-            "memory": torch.cuda.get_device_properties(0).total_memory / 1e9  # in GB
-        }
-
-    print(f"Starting experiment: {exp_id}")
-
-    hyperparameters = {
-        "predicted_iou_threshold": 0.9,
-        "stability_score_threshold": 0.8,
-        "clip_threshold": 0.9,
-        "query": "polyp"
-    }
-
-    # Get system info
-    system_info = {
-        "platform": platform.platform(),
-        "machine": platform.machine(),
-        "processor": platform.processor(),
-        "num_cores": psutil.cpu_count(),
-        "frequency": psutil.cpu_freq(),
-        "gpu": gpu_info
-    }
-    print(f"System info: {system_info}")
-
-    # Create log folder with the name of the experiment ID and include the GPU model name
-    log_folder_path = Path(f"{exp_id}_{gpu_info.get('name', platform.processor())}_{gpu_info.get('count', psutil.cpu_count)}").resolve()
-    print(f"Creating log folders at {log_folder_path}")
-    log_folder_path.mkdir(parents=True, exist_ok=True)
-
-    with open(Path(log_folder_path).joinpath("hyperparameters.json"), 'w') as f:
-        json.dump(hyperparameters, f)
-
-    with open(Path(log_folder_path).joinpath("system_info.json"), 'w') as f:
-        json.dump(system_info, f)
-
+def run_segment_video(log_folder_path):
     # Run script in another process called resource_monitor.py and send in the path of the log folder
     resource_monitor_process = subprocess.Popen([sys.executable, "resource_monitor.py", "--log_dir", str(log_folder_path)])
+
+    with open(Path(log_folder_path).joinpath("hyperparameters.json")) as f:
+        hyperparameters = json.load(f)
 
     segment_video(
         predicted_iou_threshold=hyperparameters["predicted_iou_threshold"],
@@ -95,4 +29,6 @@ def run_segment_video():
 
 
 if __name__ == '__main__':
-    run_segment_video()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log_folder_path")
+    run_segment_video(parser.log_folder_path)
